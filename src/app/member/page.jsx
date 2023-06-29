1173652a38a82de503e63e1a9b83dba60b8eeb27
jinhyunmi <hunmi961119@gmail.com>
2023-06-29 11:01:43 +0900

    23.06.29

--- src/app/member/page.jsx
+++ src/app/member/page.jsx
@@ -1,11 +1,7 @@
-import React from 'react'
+import React from "react";
 
 const Member = () => {
-  return (
-    <div>
-      Member
-    </div>
-  )
-}
+  return <div>Member</div>;
+};
 
-export default Member
+export default Member;
